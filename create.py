96f4d4baae5aa87181e9e181e8de26b4ec4551d2
Mayank Pandey <mp.pandey86@gmail.com>
2020-07-18 03:26:31 +0530

    changes

--- create.py
+++ create.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
